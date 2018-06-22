0873e3ef2579a747f0c0d1896719713d71e44e7c
Daylan Smith <daylan9046@gmail.com>
2018-06-22 08:13:03 -0500

    Added assembly metadata

--- DynamicTable/Properties/AssemblyInfo.cs
+++ DynamicTable/Properties/AssemblyInfo.cs
@@ -6,11 +6,11 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("DynamicTable")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyDescription("ASP Table that automatically fills rows with TextBoxes and gathers information from them")]
+[assembly: AssemblyConfiguration("Debug")]
+[assembly: AssemblyCompany("Hutchens Induestries")]
 [assembly: AssemblyProduct("DynamicTable")]
-[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyCopyright("Copyright © Hutchens Industries 2018")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyFileVersion("1.0.0.1")]
